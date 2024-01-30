81e46fbb056df6d2655132c1aec7b9866d61834c
kavya191 <kavyabose191@gmail.com>
2024-01-30 22:15:11 +0530

    csv file upload

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -7,47 +7,59 @@ import { Link } from "react-router-dom";
 const Login = () => {
   return (
     <section className="row">
-      <div className="column1">
-        <h1>signup</h1>
-      </div>
+      <div className="column1"></div>
       <div className="column2">
         <h2 className="title">Sign In</h2>
         <h4 className="para">Sign in to your account</h4>
         <div className="btn_style">
-          <Button className="google_btn">
-            {" "}
-            <FcGoogle /> Sign in with Google
-          </Button>
-          <Button className="apple_btn">
-            {" "}
-            <FaApple /> Sign in with Apple
-          </Button>
+          <div>
+            <Button className="google_btn">
+              {" "}
+              <FcGoogle /> Sign in with Google
+            </Button>
+            <Button className="apple_btn">
+              {" "}
+              <FaApple /> Sign in with Apple
+            </Button>
+          </div>
         </div>
+
         <Form className="login_form">
-          <Form.Label htmlFor="inputPassword5" className="text-white">
+          <Form.Label htmlFor="inputPassword5" className="text-black">
             Email
           </Form.Label>
           <Form.Control
-          className="login_input" 
+            className="login_input"
             name="email"
             type="email"
             id="emailId"
             aria-describedby="passwordHelpBlock"
           />
-          {/* {!emailValid && <p className="text-danger">* invalid email !</p>} */}
-          <Form.Label htmlFor="inputPassword5" className="text-white">
+
+          <Form.Label htmlFor="inputPassword5" className="text-black">
             Password
           </Form.Label>
-          <Form.Control name="psw" type="password" id="inputPassword5" className="login_input" />
-          {/* {!pswValid && <p className="text-danger">* invalid password !</p>} */}
-          <p className="mt-3"><Link className="text-decoration-none ">Forgot Password ?</Link></p>
+          <Form.Control
+            name="psw"
+            type="password"
+            id="inputPassword5"
+            className="login_input"
+          />
+
+          <p className="mt-3">
+            <Link className="text-decoration-none ">Forgot Password ?</Link>
+          </p>
           <div className="login_bttn">
-          <Button >Sign In</Button>
+            <Link to={"/upload"} className="text-decoration-none">
+              {" "}
+              <Button>Sign In</Button>
+            </Link>
           </div>
 
-          
-
-          <p className="text-black pt-5 d-flex justify-content-center ">Don't have an account? <Link className="text-decoration-none">Register Here</Link></p>
+          <p className="text-black pt-5 d-flex justify-content-center ">
+            Don't have an account?{" "}
+            <Link className="text-decoration-none">Register Here</Link>
+          </p>
         </Form>
       </div>
     </section>
--- src/Pages/Upload/Upload.js
+++ src/Pages/Upload/Upload.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../Pages/Upload/Upload.css";
 import { MdDashboard } from "react-icons/md";
 import { RiContactsBookUploadFill } from "react-icons/ri";
@@ -10,37 +10,76 @@ import { AiFillSchedule } from "react-icons/ai";
 import { FaUserCircle } from "react-icons/fa";
 import { PiMicrosoftExcelLogoFill } from "react-icons/pi";
 import { LuUpload } from "react-icons/lu";
+import { Table } from "react-bootstrap";
+
 function Upload() {
+  //state  to hold csv file
+  const [file, setFile] = useState();
+  //state to  hold uploaded csv file
+  const [csvArray, setCsvArray] = useState([]);
+  const fileReader = new FileReader();
+
+  //function
+  const handleChange = (e) => {
+    setFile(e.target.files[0]);
+  };
+
+  //convert csv file to readable format
+  const csvFileToArray = (string) => {
+    const csvHeader = string.slice(0, string.indexOf("\n")).split(",");
+    const csvRows = string.slice(string.indexOf("\n") + 1).split("\n");
+    const csvArray = csvRows.map((i) => {
+      const values = i.split(",");
+      const obj = csvHeader.reduce((object, header, index) => {
+        object[header] = values[index];
+        return object;
+      }, {});
+      return obj;
+    });
+    setCsvArray(csvArray);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (file) {
+      fileReader.onload = (e) => {
+        const csvOutput = e.target.result;
+        csvFileToArray(csvOutput);
+        console.log(csvOutput);
+      };
+      fileReader.readAsText(file);
+    }
+  };
+  const headerKeys = Object.keys(Object.assign({}, ...csvArray));
   return (
     <div className="main_dashboard">
       <div className="left_sideBar">
         <h2 className="d-flex justify-content-center mt-5">Base</h2>
 
-        <div className="mt-3 ms-3  me-2">
+        <div className="mt-3 ms-3  me-2 btn_upload">
           {" "}
           <MdDashboard /> Dashboard
         </div>
-        <div className="mt-4 ms-3  me-2 btn_upload">
+        <div className="mt-4 ms-3  me-2 btn_upload ">
           {" "}
           <RiContactsBookUploadFill /> Upload
         </div>
-        <div className="mt-4 ms-3  me-2">
+        <div className="mt-4 ms-3  me-2 btn_upload">
           {" "}
           <FaFileInvoice /> Invoice
         </div>
-        <div className="mt-4 ms-3  me-2">
+        <div className="mt-4 ms-3  me-2 btn_upload">
           {" "}
           <AiFillSchedule /> Schedule
         </div>
-        <div className="mt-4 ms-3  me-2">
+        <div className="mt-4 ms-3  me-2 btn_upload">
           {" "}
           <SlCalender /> Calender
         </div>
-        <div className="mt-4 ms-3  me-2">
+        <div className="mt-4 ms-3  me-2 btn_upload">
           {" "}
           <IoNotifications /> Notification
         </div>
-        <div className="mt-4 ms-3  me-2">
+        <div className="mt-4 ms-3  me-2 btn_upload">
           {" "}
           <IoMdSettings /> Settings
         </div>
@@ -55,20 +94,52 @@ function Upload() {
           </h1>
         </div>
 
-          <form className="upload_box  ">
-            <div className="d-flex justify-content-center">
-            <input type={"file"} id={"csvFileInput"} accept={".csv"} placeholder="" />
-            
-          
-            </div>
-            <div>
-              <button className="file_up">
-                <LuUpload />
-                Upload
-              </button>
-            </div>
-          </form>
- 
+        <form className="upload_box ">
+          <div className="d-flex justify-content-center">
+            <input
+              className="input_csv"
+              type={"file"}
+              id={"csvFileInput"}
+              accept={".csv"}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <button
+              className="file_up"
+              onClick={(e) => {
+                handleSubmit(e);
+              }}
+            >
+              <LuUpload />
+              Upload
+            </button>
+          </div>
+        </form>
+        <br />
+        <h2 className="list_csv">Uploads</h2>
+
+        <table class="table table-striped">
+          <thead>
+            <tr key={"header"} >
+              {headerKeys.map((key) => (
+                <th key={key}  scope="col" >
+                  {key}
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {csvArray.map((item) => (
+              <tr key={item.id}>
+              
+                {Object.values(item).map((val, index) => (
+                  <td key={index}>{val}</td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
   );
